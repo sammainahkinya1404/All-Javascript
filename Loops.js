87b2a59db7bcedda279ea064967ca61fcab7d343
Samson Kinyanjui <sammainah98@gmail.com>
2023-07-03 23:17:42 +0300

    Loops in Javascript

---
+++ Loops.js
@@ -0,0 +1,25 @@
+//While loops
+//Pre-Coditional loops
+
+// //Variable X
+// let x=0;
+// //Codition
+// while(x<=10000){
+//     console.log(x)
+//     //increment
+//     x++;
+// }
+
+//Do while loop,Post-conditional Loop
+// let y=0;
+// do{
+//     console.log(y);
+//     y++;
+// }
+// while(y<=100000);
+
+
+//For loop
+for(let z=0;z<=100;z++){
+    console.log(z);
+}
